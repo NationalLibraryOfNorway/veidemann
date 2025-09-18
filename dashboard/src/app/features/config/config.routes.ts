140faaf88bc96b10d064c6f6b061c158bfa1935d
Andreas Børsheim <andreas.borsheim@nb.no>
2025-09-18 10:58:25 +0200

    refactor: convert components to lazy loading in routes configuration

--- dashboard/src/app/features/config/config.routes.ts
+++ dashboard/src/app/features/config/config.routes.ts
@@ -1,34 +1,38 @@
-import {ConfigurationComponent, ConfigurationsComponent} from './containers';
 import {OptionsResolver} from './services';
-import {ConfigComponent} from './containers';
-import {ConfigNavListComponent} from './containers/config-nav-list/config-nav-list.component';
-import { Routes } from '@angular/router';
-import { GuardService } from '../../core';
+import {Routes} from '@angular/router';
+import {GuardService} from '../../core';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    component: ConfigNavListComponent,
+    loadComponent: () =>
+      import('./containers/config-nav-list/config-nav-list.component').then(
+        (m) => m.ConfigNavListComponent
+      ),
   },
   {
     path: ':kind',
-    component: ConfigComponent,
-    resolve: {
-      options: OptionsResolver
-    },
+    loadComponent: () =>
+      import('./config').then((m) => m.ConfigComponent),
+    resolve: { options: OptionsResolver },
     canActivate: [GuardService],
     children: [
       {
         path: '',
-        component: ConfigurationsComponent,
+        loadComponent: () =>
+          import('./containers/configurations/configurations.component').then(
+            (m) => m.ConfigurationsComponent
+          ),
       },
       {
         path: ':id',
-        component: ConfigurationComponent,
+        loadComponent: () =>
+          import('./containers/configuration/configuration.component').then(
+            (m) => m.ConfigurationComponent
+          ),
       },
     ],
   },
-
 ];
 
