e6eaee55e334b6f5e33fb1fc134173ab74183f1c
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2026-06-17 12:45:59 +0200

    browser-controller: Add integration test for session script

--- browser-controller/internal/testcontainers/images.go
+++ browser-controller/internal/testcontainers/images.go
@@ -1,5 +1,6 @@
 package testcontainersupport
 
 const (
-	BrowserlessChrome = "browserless/chrome:1.36.0-puppeteer-3.3.0"
+	BrowserlessChrome   = "browserless/chrome:1.36.0-puppeteer-3.3.0"
+	BrowserlessChromium = "ghcr.io/browserless/chromium:v2.54.0"
 )
